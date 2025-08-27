ac31236c75e1a908a9ac2212fa4e2d2ee593b84a
issmathe <issmathe@gmail.com>
2025-08-27 15:01:44 +0200

    sdfs

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const database = require('./database.js');
-const denemeRoute=require("./routes/denemes.js")  
+const denemeRoute=require("./routes/denemes.js") 
+ 
 dotenv.config();
 
 
@@ -12,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
-const PORT = process.env.PORT ||  5000;
+const PORT = process.env.PORT ||  5001;
 
 app.use("/api",denemeRoute)
 
