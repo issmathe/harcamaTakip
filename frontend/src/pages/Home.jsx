35ea5bc336bf99d76f90123765359cffc8134625
issmathe <issmathe@gmail.com>
2025-09-28 22:31:28 +0200

    adfadsf

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Deneme from '../components/Deneme.jsx'
-
+import Harcama from '../components/Harcama.jsx'
 const Home = () => {
   return (
     <div>
       <Deneme/>
+      <Harcama/>
     </div>
   )
 }
