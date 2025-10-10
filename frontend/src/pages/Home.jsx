870e1e517188c8e583b31acf3f456af2d56170ab
issmathe <issmathe@gmail.com>
2025-10-10 16:26:29 +0200

    sdfee

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,19 +7,20 @@ import { TotalsProvider } from "../context/TotalsContext";
 const Home = () => {
   return (
     <TotalsProvider>
-      <div className="relative min-h-screen bg-gray-100 overflow-hidden">
-        {/* Üstte sabit kalan header */}
-        <div className="sticky top-0 z-[999]">
+      {/* Sabit ekran, taş gibi layout */}
+      <div className="fixed inset-0 flex flex-col bg-gray-100 overflow-hidden touch-none select-none">
+        {/* Üst sabit header */}
+        <div className="z-[999]">
           <Header />
         </div>
 
-        {/* Ana içerik */}
-        <main className="flex-1 flex justify-center pt-16 pb-24 px-4 relative z-[1]">
+        {/* Orta alan — scroll olmasın */}
+        <main className="flex-1 flex justify-center items-center relative z-[1]">
           <MainContent />
         </main>
 
-        {/* Altta sabit kalan navigasyon */}
-        <div className="fixed bottom-0 left-0 w-full z-[998]">
+        {/* Alt sabit navigasyon */}
+        <div className="z-[998]">
           <BottomNav />
         </div>
       </div>
